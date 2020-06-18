012b7803e96a75bd4d345ec704cf001b17b731b3
Guus Lieben <guus@xendox.com>
2020-06-18 22:39:20 +0200

    Add packet parsing (content, header, hash) and hash generation

---
+++ core/src/main/java/org/dockbox/corona/core/util/PacketSecurityUtil.java
@@ -0,0 +1,61 @@
+package org.dockbox.corona.core.util;
+
+import org.dockbox.corona.core.packets.Packet;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Optional;
+
+public class PacketSecurityUtil {
+
+    public static final String INVALID = "$InvalidatedContent";
+
+    public static String getHash(String unencryptedPacket) {
+        String[] packetLines = unencryptedPacket.split("\n");
+        String hashLine = packetLines[packetLines.length - 1];
+        if (hashLine.startsWith("HASH::")) return hashLine.replaceFirst("HASH::", "");
+        return PacketSecurityUtil.INVALID;
+    }
+
+    public static String getContent(String unencryptedPacket) {
+        String[] packetLines = unencryptedPacket.split("\n");
+        String[] contentLines = Arrays.copyOfRange(packetLines, 1, packetLines.length - 1);
+        return String.join("\n", contentLines);
+    }
+
+    public static String getHeader(String unencryptedPacket) {
+        return unencryptedPacket.split("\n")[0];
+    }
+
+    public static String generateHash(String content) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            byte[] messageDigest = md.digest(content.getBytes(StandardCharsets.UTF_8));
+
+            BigInteger no = new BigInteger(1, messageDigest);
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
