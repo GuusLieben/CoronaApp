8205f0e10a487e19cb2ea80e8ca3d8cf9d9eb934
Guus Lieben <guus@xendox.com>
2020-06-18 13:48:11 +0200

    Add common utilities

---
+++ core/src/main/java/org/dockbox/corona/core/util/CommonUtil.java
@@ -0,0 +1,16 @@
+package org.dockbox.corona.core.util;
+
+import java.sql.Time;
+import java.time.Instant;
+
+public class CommonUtil {
+
+    public static String parseTimeString(Time time) {
+        return "";
+    }
+
+    public static Time parseTime(String time) {
+        return Time.valueOf(time);
+    }
+
+}
