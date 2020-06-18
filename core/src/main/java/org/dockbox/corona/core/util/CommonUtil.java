24d551711d3c2b3bea7ee047d27fd940932e862a
Guus Lieben <guus@xendox.com>
2020-06-18 22:42:19 +0200

    Temporarily fix time/date parsers (implementation required)

--- core/src/main/java/org/dockbox/corona/core/util/CommonUtil.java
+++ core/src/main/java/org/dockbox/corona/core/util/CommonUtil.java
@@ -3,22 +3,24 @@ package org.dockbox.corona.core.util;
 import java.sql.Date;
 import java.sql.Time;
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class CommonUtil {
 
     public static String parseTimeString(Time time) {
-        return "";
+        return "${timestamp}";
     }
 
     public static Time parseTime(String time) {
-        return Time.valueOf(time);
+        return Time.valueOf(LocalTime.NOON);
     }
 
     public static String parseDateString(Date date) {
-        return "";
+        return "${date}";
     }
 
     public static Date parseDate(String date) {
-        return Date.valueOf(date);
+        return Date.valueOf(LocalDate.now());
     }
 }
