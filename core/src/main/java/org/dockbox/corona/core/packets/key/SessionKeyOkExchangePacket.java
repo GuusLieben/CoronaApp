bb4f86579a6192d4e5292a722b16e10342c4eb90
Guus Lieben <guus@xendox.com>
2020-06-19 19:52:15 +0200

    Add key exchange packets

---
+++ core/src/main/java/org/dockbox/corona/core/packets/key/SessionKeyOkExchangePacket.java
@@ -0,0 +1,42 @@
+package org.dockbox.corona.core.packets.key;
+
+import org.dockbox.corona.core.packets.Packet;
+import org.dockbox.corona.core.util.Util;
+
+import javax.crypto.SecretKey;
+import java.security.KeyException;
+
+public class SessionKeyOkExchangePacket extends Packet {
+
+    public static final SessionKeyOkExchangePacket EMPTY = new SessionKeyOkExchangePacket(null);
+
+    private final SecretKey sessionKey;
+
+    public SessionKeyOkExchangePacket(SecretKey sessionKey) {
+        this.sessionKey = sessionKey;
+    }
+
+    @Override
+    public String getHeader() {
+        return "KEY::SESSION_OK";
+    }
+
+    @Override
+    public String serialize() {
+        return "KEY::SESSION_OK::" + Util.encodeKeyToBase64(sessionKey);
+    }
+
+    @Override
+    public Packet deserialize(String message) {
+        String base64EncodedKey = message.replaceFirst("KEY::SESSION_OK::", "");
+        try {
+            return new SessionKeyOkExchangePacket((SecretKey) Util.decodeBase64ToKey(base64EncodedKey, true));
+        } catch (KeyException e) {
+            return null;
+        }
+    }
+
+    public SecretKey getSessionKey() {
+        return sessionKey;
+    }
+}
