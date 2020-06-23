c79e8937c29f8768a316532c74bc07bd63bcdcb8
Guus Lieben <guus@xendox.com>
2020-06-23 11:51:47 +0200

    Add logging to listeners

--- core/src/main/java/org/dockbox/corona/core/network/NetworkListener.java
+++ core/src/main/java/org/dockbox/corona/core/network/NetworkListener.java
@@ -5,11 +5,12 @@ import org.dockbox.corona.core.packets.key.PublicKeyExchangePacket;
 import org.dockbox.corona.core.packets.key.SessionKeyExchangePacket;
 import org.dockbox.corona.core.packets.key.SessionKeyOkExchangePacket;
 import org.dockbox.corona.core.util.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.crypto.SecretKey;
 import java.io.IOException;
 import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.security.PrivateKey;
 import java.util.Map;
@@ -18,43 +19,55 @@ import java.util.concurrent.ConcurrentHashMap;
 public abstract class NetworkListener extends NetworkCommunicator {
 
     private final PrivateKey privateKey;
-    private final DatagramSocket socket;
     private final Map<String, Session> sessions = new ConcurrentHashMap<>();
 
     public NetworkListener(PrivateKey privateKey) throws IOException, InstantiationException {
         super(privateKey);
         this.privateKey = privateKey;
-        this.socket = new DatagramSocket();
     }
 
     public void listen() {
+        log.info("Opening socket on port " + getSocket().getLocalPort());
         while (true) {
             try {
+                log.info("Preparing buffer");
                 byte[] buffer = new byte[Util.INITIAL_KEY_BLOCK_SIZE];
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                socket.receive(packet);
+                log.info("Waiting for packets on port " + getSocket().getLocalPort());
+                getSocket().receive(packet);
+
                 String rawPacket = Util.convertPacketBytes(packet.getData());
                 String remoteLocation = String.format("%s:%d", packet.getAddress().getHostAddress(), packet.getPort());
+                log.info("Received packet from " + remoteLocation);
 
                 if (rawPacket.startsWith(KeyHeaders.KEY_PREFIX.getValue())) {
+                    log.info("Key exchange packet, validating");
                     // Plain text, no decryption needed
                     if (rawPacket.startsWith(PublicKeyExchangePacket.EMPTY.getHeader())) {
                         PublicKeyExchangePacket pkep = PublicKeyExchangePacket.EMPTY.deserialize(rawPacket);
-                        if (pkep != null) sendDatagram(KeyHeaders.KEY_OK.getValue(), true, packet.getAddress(), packet.getPort(), false);
-
-                        else sendDatagram(KeyHeaders.KEY_REJECTED.getValue(), true, packet.getAddress(), packet.getPort(), false);
+                        if (pkep != null) {
+                            log.info("Public key OK");
+                            sendDatagram(KeyHeaders.KEY_OK.getValue(), true, packet.getAddress(), packet.getPort(), false);
+                        } else {
+                            log.info("Public key rejected");
+                            sendDatagram(KeyHeaders.KEY_REJECTED.getValue(), true, packet.getAddress(), packet.getPort(), false);
+                        }
 
                     } else if (rawPacket.startsWith(SessionKeyExchangePacket.EMPTY.getHeader())) {
                         SessionKeyExchangePacket skep = SessionKeyExchangePacket.EMPTY.deserialize(rawPacket);
                         if (skep != null && Util.sessionKeyIsValid(skep.getSessionKey(), privateKey)) {
+                            log.info("Session key OK");
                             SessionKeyOkExchangePacket skoep = new SessionKeyOkExchangePacket(skep.getSessionKey());
                             sendPacket(skoep, true, packet.getAddress(), packet.getPort());
                             sessions.put(remoteLocation, new Session(skoep.getSessionKey(), packet.getAddress(), packet.getPort()));
-
-                        } else sendDatagram(KeyHeaders.KEY_REJECTED.getValue(), true, packet.getAddress(), packet.getPort(), false);
+                        } else {
+                            log.info("Session key rejected");
+                            sendDatagram(KeyHeaders.KEY_REJECTED.getValue(), true, packet.getAddress(), packet.getPort(), false);
+                        }
                     }
 
                 } else {
+                    log.info("Encrypted packet, decrypting in session");
                     // Encrypted
                     Session session = sessions.get(remoteLocation);
                     new Thread(session.injectPacket(rawPacket)).start();
@@ -87,6 +100,7 @@ public abstract class NetworkListener extends NetworkCommunicator {
         private final SecretKey sessionKey;
         private final InetAddress remote;
         private final int remotePort;
+        private final Logger log;
 
         private String rawPacket;
 
@@ -99,6 +113,7 @@ public abstract class NetworkListener extends NetworkCommunicator {
             this.sessionKey = sessionKey;
             this.remote = remote;
             this.remotePort = remotePort;
+            this.log = LoggerFactory.getLogger(remote.getHostAddress() + "-session");
         }
 
         public SecretKey getSessionKey() {
@@ -119,6 +134,7 @@ public abstract class NetworkListener extends NetworkCommunicator {
 
         @Override
         public void run() {
+            log.info("Session started");
             handlePacket(getRawPacket(), this);
         }
     }
