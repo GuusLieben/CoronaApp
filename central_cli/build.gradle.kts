a9a41fe029b7991bd5ec2c87b68fc810dbad902b
Guus Lieben <guus@xendox.com>
2020-06-23 17:27:46 +0200

    Add SLF4J on all projects

--- central_cli/build.gradle.kts
+++ central_cli/build.gradle.kts
@@ -11,4 +11,6 @@ dependencies {
     compile(project(":core"))
     compile(kotlin("stdlib"))
     compile("com.microsoft.sqlserver:mssql-jdbc:8.2.2.jre8")
+    implementation("org.slf4j", "slf4j-api", "1.7.25")
+    implementation("org.slf4j", "slf4j-simple", "1.7.25")
 }
