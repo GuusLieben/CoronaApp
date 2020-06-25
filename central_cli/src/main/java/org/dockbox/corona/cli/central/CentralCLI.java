367470cdc0cc3fc927163b426172160b37fbfc1e
Guus Lieben <guus@xendox.com>
2020-06-25 10:44:02 +0200

    Auto start CCLI

--- central_cli/src/main/java/org/dockbox/corona/cli/central/CentralCLI.java
+++ central_cli/src/main/java/org/dockbox/corona/cli/central/CentralCLI.java
@@ -1,19 +1,20 @@
 package org.dockbox.corona.cli.central;
 
+import org.dockbox.corona.cli.central.network.CLINetworkListener;
 import org.dockbox.corona.core.util.Util;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.security.PrivateKey;
 
 public class CentralCLI {
 
-    public static final Logger log = LoggerFactory.getLogger(CentralCLI.class);
     public static final PrivateKey CENTRAL_CLI_PRIVATE = Util.storePubAndGetKey(getPublicKeyFile()).get();
     public static final int LISTENER_PORT = 9191;
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        CLINetworkListener listener = new CLINetworkListener();
+        listener.listen();
+    }
 
     public static File getPublicKeyFile() {
         return new File("central_cli.pub");
